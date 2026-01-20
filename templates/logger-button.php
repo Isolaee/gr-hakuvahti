d437bcd45a91909a588471b53eb0c9143989c23b
isolaee <eero.isola@gmail.com>
2026-01-20 13:57:23 +0200

    Remove Hakuvahti modal and associated popup functionality

--- templates/logger-button.php
+++ templates/logger-button.php
@@ -2,23 +2,5 @@
 // Hakuvahti button and modal template
 ?>
 <?php if ( is_user_logged_in() ) : ?>
-<div class="acf-analyzer-buttons">
-    <button class="acf-hakuvahti-save">
-        <?php esc_html_e( 'Hakuvahti', 'acf-analyzer' ); ?>
-    </button>
-</div>
-
-<!-- Hakuvahti Save Modal -->
-<div id="hakuvahti-save-modal" class="hakuvahti-modal" aria-hidden="true" style="display: none;">
-    <div class="hakuvahti-modal-content">
-        <form id="hakuvahti-save-form">
-            <p class="hakuvahti-info"><?php esc_html_e( 'Luo hakuvahti valitsemalla hakuehdot:', 'acf-analyzer' ); ?></p>
-            <div id="hakuvahti-criteria-preview" aria-live="polite"></div>
-            <p><?php esc_html_e( 'Anna nimi Hakuvahdille:', 'acf-analyzer' ); ?></p>
-            <input type="text" id="hakuvahti-name" name="name" required placeholder="<?php esc_attr_e( 'Esim. Omat hakuehdot', 'acf-analyzer' ); ?>">
-            <button type="submit" class="hakuvahti-submit"><?php esc_html_e( 'Tallenna', 'acf-analyzer' ); ?></button>
-        </form>
-        <div id="hakuvahti-save-message"></div>
-    </div>
-</div>
+    <!-- Hakuvahti popup removed -->
 <?php endif; ?>
